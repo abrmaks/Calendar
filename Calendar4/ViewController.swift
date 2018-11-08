50e45e609c5ffd371f8931451ab2fe8ea6e01271
Abrikosov Maksim <max@maxs-macbook-pro.local>
2018-11-08 09:29:19 +0300

    Initial Commit

---
+++ Calendar4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calendar4
+//
+//  Created by  max on 08/11/2018.
+//  Copyright © 2018  max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
